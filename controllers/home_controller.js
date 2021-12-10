67290a544692ddd4ffc7743f95a72fce29121c60
saniya <saniyafarzin75@gmail.com>
2021-12-10 22:38:31 +0530

    setting up the view engine

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -7,6 +7,10 @@
 
 //here home is the NAME of the FUNCTION
 module.exports.home = function(req, res){
-    return res.end('<h1>Express is up for Codeial</h1>');
+    //return res.end('<h1>Express is up for Codeial</h1>');
+
+    return res.render('home', {
+        title : "Home"
+    });
 }
 //after creating the controller, access it in the router file
