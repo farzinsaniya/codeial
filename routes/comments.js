f3e9f73d44be43dfbd2e63b503856a335dea8f2b
saniya <saniyafarzin75@gmail.com>
2021-12-21 20:02:32 +0530

    add delete comment

--- routes/comments.js
+++ routes/comments.js
@@ -4,7 +4,9 @@ const passport = require('passport');
 
 const commentsController = require('../controllers/comments_controller');
 
+//creating or adding a comment
 router.post('/create', passport.checkAuthentication, commentsController.create);
-
+//deleting or destroyin the comment
+router.get('/destroy/:id', passport.checkAuthentication, commentsController.destroy);
 
 module.exports = router;
