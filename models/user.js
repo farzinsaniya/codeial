ef7c50206d2b16abaf746ee9cb62fb018dd6e87d
saniya <saniyafarzin75@gmail.com>
2022-01-06 17:50:35 +0530

    add multer

--- models/user.js
+++ models/user.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const multer = require('multer');
+const path = require('path');
+const AVATAR_PATH = path.join('/uploads/users/avatars'); 
+
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -13,11 +17,28 @@ const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
+    },
+    avatar: {
+        type: String
     }
-}, {
+},{
     timestamps: true
 });
 
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, '..', AVATAR_PATH))
+    },
+    filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix);
+    }
+});
+
+//defining static functions
+//multer({storage: storage}).single('avatar') defines that only 1 instance shall be created for uploading a single file
+userSchema.statics.uploadedAvatar = multer({storage: storage}).single('avatar');
+userSchema.statics.avatarPath = AVATAR_PATH;
 
 const User = mongoose.model('User', userSchema);
 
