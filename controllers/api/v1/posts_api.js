75fceb89735873f6f0d58c2f9672f208876ee2ba
saniya <saniyafarzin75@gmail.com>
2022-01-11 17:57:47 +0530

    add delete posts auth

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -26,7 +26,7 @@ module.exports.destroy = async function(req, res){
         let post = await Post.findById(req.params.id);
 
         // .id means converting the object id into string
-    //    if (post.user == req.user.id){
+        if (post.user == req.user.id){
             post.remove();
 
         await Comment.deleteMany({post: req.params.id});
@@ -42,10 +42,13 @@ module.exports.destroy = async function(req, res){
         return res.json(200, {
             message: "Posts & its associated comments deleted successfully!"
         });
-        // }else{
-        //     req.flash('error','You cannot delete this post!');
-        //     return res.redirect('back');
-        // }
+        }else{
+            // req.flash('error','You cannot delete this post!');
+            // return res.redirect('back');
+            return res.json(401, {
+                message: "You cannot delete this post!"
+            });
+        }
     } catch (err) {
         // req.flash('error', err);
         console.log('********', err);
